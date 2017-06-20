e71d2a3b66722c7fdd6dbcab44e663b496c1b9f5
YamatoAndo <yamato.ando@gmail.com>
2017-06-20 14:28:42 +0900

    add support for /twist_cmd, /mode_cmd, /gear_cmd, /accel_cmd, /steer_cmd, /brake_cmd and /ctrl_cmd

--- ros/src/socket/packages/mqtt_socket/nodes/select_command/select_command.cpp
+++ ros/src/socket/packages/mqtt_socket/nodes/select_command/select_command.cpp
@@ -31,33 +31,54 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <map>
 
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
+#include <geometry_msgs/TwistStamped.h>
+
 #include "mqtt_socket/RemoteCmd.h"
+#include "mqtt_socket/SelectCmd.h"
+#include "tablet_socket_msgs/mode_cmd.h"
+#include "tablet_socket_msgs/gear_cmd.h"
+#include "runtime_manager/accel_cmd.h"
+#include "runtime_manager/brake_cmd.h"
+#include "runtime_manager/steer_cmd.h"
+#include "waypoint_follower_msgs/ControlCommandStamped.h"
 
 class SelectCommand
 {
   using remote_msgs_t = mqtt_socket::RemoteCmd;
-  using auto_msgs_t = mqtt_socket::RemoteCmd;  //TODO fix message type
+  //using auto_msgs_t = mqtt_socket::RemoteCmd;  //TODO fix message type
+  using select_msgs_t = mqtt_socket::SelectCmd;
 
   public:
     SelectCommand(const ros::NodeHandle& nh, const ros::NodeHandle& private_nh);
     ~SelectCommand();
   private:
     void watchdog_timer();
-    void remote_command_callback(const remote_msgs_t::ConstPtr& input);
-    void auto_command_callback(const auto_msgs_t::ConstPtr& input);
+    void remote_cmd_callback(const remote_msgs_t::ConstPtr& input_msg);
+    void auto_cmd_twist_cmd_callback(const geometry_msgs::TwistStamped::ConstPtr& input_msg);
+    void auto_cmd_mode_cmd_callback(const tablet_socket_msgs::mode_cmd::ConstPtr& input_msg);
+    void auto_cmd_gear_cmd_callback(const tablet_socket_msgs::gear_cmd::ConstPtr& input_msg);
+    void auto_cmd_accel_cmd_callback(const runtime_manager::accel_cmd::ConstPtr& input_msg);
+    void auto_cmd_steer_cmd_callback(const runtime_manager::steer_cmd::ConstPtr& input_msg);
+    void auto_cmd_brake_cmd_callback(const runtime_manager::brake_cmd::ConstPtr& input_msg);
+    void auto_cmd_ctrl_cmd_callback(const waypoint_follower_msgs::ControlCommandStamped::ConstPtr& input_msg);
+
+    void reset_select_cmd_msg();
 
     ros::NodeHandle nh_;
     ros::NodeHandle private_nh_; 
     ros::Publisher emergency_stop_pub_;
-    ros::Publisher select_command_pub_;
-    ros::Subscriber remote_command_sub_;
-    ros::Subscriber auto_command_sub_;
-    ros::Time remote_command_time_;
+    ros::Publisher select_cmd_pub_;
+    ros::Subscriber remote_cmd_sub_;
+    std::map<std::string , ros::Subscriber> auto_cmd_sub_stdmap_;
+
+    select_msgs_t select_cmd_msg_;
+    std_msgs::Bool emergency_stop_msg_;
+    ros::Time remote_cmd_time_;
     ros::Duration timeout_period_;
-    std_msgs::Bool emergency_stop_;
 
     std::thread watchdog_timer_thread_;
     enum class CommandMode{AUTO=1, REMOTE} command_mode_;
@@ -70,13 +91,23 @@ SelectCommand::SelectCommand(const ros::NodeHandle& nh, const ros::NodeHandle& p
     ,command_mode_(CommandMode::REMOTE)
 {
   emergency_stop_pub_ = nh_.advertise<std_msgs::Bool>("/emergency_stop", 1, true);
-  select_command_pub_ = nh_.advertise<remote_msgs_t>("/select_cmd", 1, true);
-  remote_command_sub_ = nh_.subscribe("/remote_cmd", 1, &SelectCommand::remote_command_callback, this);
-  auto_command_sub_ = nh_.subscribe("/auto_cmd", 1, &SelectCommand::auto_command_callback, this);
+  select_cmd_pub_ = nh_.advertise<select_msgs_t>("/select_cmd", 1, true);
 
-  emergency_stop_.data = false;
-  
-  remote_command_time_ = ros::Time::now();
+  remote_cmd_sub_ = nh_.subscribe("/remote_cmd", 1, &SelectCommand::remote_cmd_callback, this);
+
+  auto_cmd_sub_stdmap_["twist_cmd"] = nh_.subscribe("/twist_cmd", 1, &SelectCommand::auto_cmd_twist_cmd_callback, this);
+  auto_cmd_sub_stdmap_["mode_cmd"] = nh_.subscribe("/mode_cmd", 1, &SelectCommand::auto_cmd_mode_cmd_callback, this);
+  auto_cmd_sub_stdmap_["gear_cmd"] = nh_.subscribe("/gear_cmd", 1, &SelectCommand::auto_cmd_gear_cmd_callback, this);
+  auto_cmd_sub_stdmap_["accel_cmd"] = nh_.subscribe("/accel_cmd", 1, &SelectCommand::auto_cmd_accel_cmd_callback, this);
+  auto_cmd_sub_stdmap_["steer_cmd"] = nh_.subscribe("/steer_cmd", 1, &SelectCommand::auto_cmd_steer_cmd_callback, this);
+  auto_cmd_sub_stdmap_["brake_cmd"] = nh_.subscribe("/brake_cmd", 1, &SelectCommand::auto_cmd_brake_cmd_callback, this);
+  auto_cmd_sub_stdmap_["ctrl_cmd"] = nh_.subscribe("/ctrl_cmd", 1, &SelectCommand::auto_cmd_ctrl_cmd_callback, this);
+
+  select_cmd_msg_.header.seq = 0;
+
+  emergency_stop_msg_.data = false;
+
+  remote_cmd_time_ = ros::Time::now();
   watchdog_timer_thread_ = std::thread(&SelectCommand::watchdog_timer, this);
   watchdog_timer_thread_.detach();
 }
@@ -85,43 +116,150 @@ SelectCommand::~SelectCommand()
 {
 }
 
+void SelectCommand::reset_select_cmd_msg()
+{
+  select_cmd_msg_.linear_x        = 0;
+  select_cmd_msg_.angular_z       = 0;
+  select_cmd_msg_.mode            = 0;
+  select_cmd_msg_.gear            = 0;
+  select_cmd_msg_.accel           = 0;
+  select_cmd_msg_.brake           = 0;
+  select_cmd_msg_.steer           = 0;
+  select_cmd_msg_.linear_velocity = -1;
+  select_cmd_msg_.steering_angle  = 0;
+}
+
 void SelectCommand::watchdog_timer()
 {
   while(1)
   {
     ros::Time now_time = ros::Time::now();
 
-    if(now_time - remote_command_time_ >  timeout_period_ 
-       || emergency_stop_.data == true)
+    if(now_time - remote_cmd_time_ >  timeout_period_ 
+       || emergency_stop_msg_.data == true)
     {
         command_mode_ = CommandMode::AUTO;
-        emergency_stop_.data = true;
-        emergency_stop_pub_.publish(emergency_stop_);
+        emergency_stop_msg_.data = true;
+        emergency_stop_pub_.publish(emergency_stop_msg_);
     }
 
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    std::cout << "c_mode:"     << static_cast<int>(command_mode_) 
-              << " e_stop:"    << static_cast<bool>(emergency_stop_.data)
-              << " diff_time:" << (now_time - remote_command_time_).toSec() 
+    std::cout << "c_mode:"     << static_cast<int>(command_mode_)
+              << " e_stop:"    << static_cast<bool>(emergency_stop_msg_.data)
+              << " diff_time:" << (now_time - remote_cmd_time_).toSec()
               << std::endl;
   }
 }
 
-void SelectCommand::remote_command_callback(const remote_msgs_t::ConstPtr& input_msgs)
+void SelectCommand::remote_cmd_callback(const remote_msgs_t::ConstPtr& input_msg)
 {
-  command_mode_ = static_cast<CommandMode>(input_msgs->mode);
-  emergency_stop_.data = static_cast<bool>(input_msgs->emergency);
-  remote_command_time_ = ros::Time::now();
-  
+  command_mode_ = static_cast<CommandMode>(input_msg->mode);
+  select_cmd_msg_.mode = input_msg->mode;
+  emergency_stop_msg_.data = static_cast<bool>(input_msg->emergency);
+  remote_cmd_time_ = ros::Time::now();
+
   if(command_mode_ == CommandMode::REMOTE)
-    select_command_pub_.publish(*input_msgs);
+  {
+    select_cmd_msg_.header.frame_id = input_msg->header.frame_id;
+    select_cmd_msg_.header.stamp = input_msg->header.stamp;
+    select_cmd_msg_.header.seq++;
+    select_cmd_msg_.accel = input_msg->accel;
+    select_cmd_msg_.brake = input_msg->brake;
+    select_cmd_msg_.steer = input_msg->steer;
+    select_cmd_msg_.gear = input_msg->gear;
+    select_cmd_msg_.mode = input_msg->mode;
+    select_cmd_msg_.emergency = input_msg->emergency;
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
 }
 
-void SelectCommand::auto_command_callback(const auto_msgs_t::ConstPtr& input_msgs)
+void SelectCommand::auto_cmd_twist_cmd_callback(const geometry_msgs::TwistStamped::ConstPtr& input_msg)
 {
   if(command_mode_ == CommandMode::AUTO)
-    select_command_pub_.publish(*input_msgs);
+  {
+    select_cmd_msg_.header.frame_id = input_msg->header.frame_id;
+    select_cmd_msg_.header.stamp = input_msg->header.stamp;
+    select_cmd_msg_.header.seq++;
+    select_cmd_msg_.linear_x = input_msg->twist.linear.x;
+    select_cmd_msg_.angular_z = input_msg->twist.angular.z;
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
 }
+
+void SelectCommand::auto_cmd_mode_cmd_callback(const tablet_socket_msgs::mode_cmd::ConstPtr& input_msg)
+{
+  if(command_mode_ == CommandMode::AUTO)
+  {
+    //TODO:check this if statement
+    if(input_msg->mode == -1 || input_msg->mode == 0){
+      reset_select_cmd_msg();
+    }
+    select_cmd_msg_.header.frame_id = input_msg->header.frame_id;
+    select_cmd_msg_.header.stamp = input_msg->header.stamp;
+    select_cmd_msg_.header.seq++;
+    select_cmd_msg_.mode = input_msg->mode; 
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
+}
+
+void SelectCommand::auto_cmd_gear_cmd_callback(const tablet_socket_msgs::gear_cmd::ConstPtr& input_msg)
+{
+  if(command_mode_ == CommandMode::AUTO)
+  {
+    select_cmd_msg_.gear = input_msg->gear;
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
+}
+
+void SelectCommand::auto_cmd_accel_cmd_callback(const runtime_manager::accel_cmd::ConstPtr& input_msg)
+{
+  if(command_mode_ == CommandMode::AUTO)
+  {
+    select_cmd_msg_.header.frame_id = input_msg->header.frame_id;
+    select_cmd_msg_.header.stamp = input_msg->header.stamp;
+    select_cmd_msg_.header.seq++;
+    select_cmd_msg_.accel = input_msg->accel;
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
+}
+
+void SelectCommand::auto_cmd_steer_cmd_callback(const runtime_manager::steer_cmd::ConstPtr& input_msg)
+{
+  if(command_mode_ == CommandMode::AUTO)
+  {
+    select_cmd_msg_.header.frame_id = input_msg->header.frame_id;
+    select_cmd_msg_.header.stamp = input_msg->header.stamp;
+    select_cmd_msg_.header.seq++;
+    select_cmd_msg_.steer = input_msg->steer;
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
+}
+
+void SelectCommand::auto_cmd_brake_cmd_callback(const runtime_manager::brake_cmd::ConstPtr& input_msg)
+{
+  if(command_mode_ == CommandMode::AUTO)
+  {
+    select_cmd_msg_.header.frame_id = input_msg->header.frame_id;
+    select_cmd_msg_.header.stamp = input_msg->header.stamp;
+    select_cmd_msg_.header.seq++;
+    select_cmd_msg_.brake = input_msg->brake;
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
+}
+
+void SelectCommand::auto_cmd_ctrl_cmd_callback(const waypoint_follower_msgs::ControlCommandStamped::ConstPtr& input_msg)
+{
+  if(command_mode_ == CommandMode::AUTO)
+  {
+    select_cmd_msg_.header.frame_id = input_msg->header.frame_id;
+    select_cmd_msg_.header.stamp = input_msg->header.stamp;
+    select_cmd_msg_.header.seq++;
+    select_cmd_msg_.linear_velocity = input_msg->cmd.linear_velocity;
+    select_cmd_msg_.steering_angle = input_msg->cmd.steering_angle;
+    select_cmd_pub_.publish(select_cmd_msg_);
+  }
+}
+
 
 int main(int argc, char** argv)
 {
