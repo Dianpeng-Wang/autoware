6cbba599e43e8a886099bc6c937a22ceb29debd4
AMC <abrahammonrroy@yahoo.com>
2017-07-24 07:41:55 +0900

    Topics synced -Image Detections -LiDAR Detections
    TODO:
    -Subscribe to Extrinsic Matrix
    -Match image and LiDAR detections

--- ros/src/computing/perception/detection/packages/camera_lidar_fuser/src/camera_lidar_fuser.cpp
+++ ros/src/computing/perception/detection/packages/camera_lidar_fuser/src/camera_lidar_fuser.cpp
@@ -15,21 +15,6 @@
 #include "autoware_msgs/DetectedObject.h"
 #include "autoware_msgs/DetectedObjectArray.h"
 
-
-class CameraLidarFuser
-{
-public:
-	CameraLidarFuser();
-
-private:
-
-	ros::NodeHandle node_handle_;
-	ros::Subscriber cloud_clusters_sub_;
-	ros::Publisher detected_objects_pub_;
-
-	void CloudClustersCallback(const autoware_msgs::CloudClusterArray::Ptr& in_cloud_cluster_array_ptr);
-};
-
 //TODO:
 //1. Sync:
 //    -Clusters
@@ -37,37 +22,63 @@ private:
 //    -Image Frame
 //    -Extrinsic Calibration
 
-CameraLidarFuser::CameraLidarFuser() :
+
+class CameraLidarFuser
+{
+private:
+
+	ros::NodeHandle node_handle_;
+	ros::Subscriber extrinsic_sub_;
+	ros::Publisher cloud_clusters_class_pub_;
+
+
+	typedef message_filters::sync_policies::ApproximateTime<autoware_msgs::CloudClusterArray,
+															autoware_msgs::DetectedObjectArray,
+															autoware_msgs::DetectedObjectArray> FusionSyncPolicy;
+
+	message_filters::Subscriber<autoware_msgs::CloudClusterArray> *cloud_clusters_sub_;
+	message_filters::Subscriber<autoware_msgs::DetectedObjectArray> *car_detection_sub_;
+	message_filters::Subscriber<autoware_msgs::DetectedObjectArray> *person_detection_sub_;
+
+	message_filters::Synchronizer<FusionSyncPolicy> *sync_;
+public:
+	CameraLidarFuser() :
 		node_handle_("~")
-{
-	cloud_clusters_sub_ = node_handle_.subscribe("/cloud_clusters_class", 10, &CameraLidarFuser::CloudClustersCallback, this);
-	detected_objects_pub_ = node_handle_.advertise<autoware_msgs::DetectedObjectArray>( "/detected_objects", 10);
-}
-
-void CameraLidarFuser::CloudClustersCallback(const autoware_msgs::CloudClusterArray::Ptr& in_cloud_cluster_array_ptr)
-{
-	autoware_msgs::DetectedObjectArray detected_objects;
-	detected_objects.header = in_cloud_cluster_array_ptr->header;
-	for (auto i = in_cloud_cluster_array_ptr->clusters.begin(); i != in_cloud_cluster_array_ptr->clusters.end(); i++)
 	{
-		autoware_msgs::DetectedObject detected_object;
-		detected_object.header 		= i->header;
-		detected_object.id 			= i->id;
-		detected_object.label 		= i->label;
-		detected_object.dimensions 	= i->bounding_box.dimensions;
-		detected_object.pose 		= i->bounding_box.pose;
-
-		detected_objects.objects.push_back(detected_object);
+		cloud_clusters_sub_ = node_handle_.subscribe("/cloud_clusters", 10, &CameraLidarFuser::CloudClustersCallback, this);
+		cloud_clusters_class_pub_ = node_handle_.advertise<autoware_msgs::CloudClusterArray>( "/detected_objects", 10);
 	}
-	detected_objects_pub_.publish(detected_objects);
-}
+
+
+	void SyncedCallback(const autoware_msgs::CloudClusterArray::ConstPtr& in_lidar_detections,
+								const autoware_msgs::DetectedObjectArray::ConstPtr& in_car_image_detections,
+								const autoware_msgs::DetectedObjectArray::ConstPtr& in_person_image_detections)
+	{
+
+	}
+
+	void Run()
+	{
+		cloud_clusters_sub_ = new message_filters::Subscriber<autoware_msgs::CloudClusterArray>(node_handle_, "cloud_clusters", 10);
+		car_detection_sub_ = new message_filters::Subscriber<autoware_msgs::DetectedObjectArray>(node_handle_, "/obj_car/image_obj", 10);
+		person_detection_sub_ = new message_filters::Subscriber<autoware_msgs::DetectedObjectArray>(node_handle_, "/obj_person/image_obj", 10);
+
+		sync_ = new message_filters::Synchronizer<FusionSyncPolicy>(TrackerS);
+
+		sync_->registerCallback(boost::bind(&CameraLidarFuser::SyncedCallback, this, _1, _2, _3));
+
+		ros::spin();
+	}
+};
 
 int main(int argc, char **argv)
 {
-
 	ros::init(argc, argv, "camera_lidar_fuser");
+
 	CameraLidarFuser node;
-	ros::spin();
+
+	node.Run();
 
 	return 0;
 }
+
